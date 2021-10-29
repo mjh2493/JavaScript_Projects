e93d9b025806471ec80d5b3436d50b61df954c45
mjh2493 <mjh2493@gmail.com>
2021-10-29 14:20:46 -0400

    Updated to JS projects

--- Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,27 @@
-alert("Hello, world!");
+var W="Waffle the Pup";
+window.alert(W);
+
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
+document.write("Hello, world! <br> \"Hi, there!\" said the World.<br>")
+var A="This is a string!<br>";
+document.write("\"Be who you are and say what you feel," 
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Suess<br>") //Showing how to combine strings.
+var M="Mike and" + " Mallory<br>";
+var Family="The Humphries", Dad="Dale", Mom="Lynn", Oldest="Mallory", Middle="Harrison", Youngest="Jenna";
+document.write(Oldest); //This calls the oldest variable to be displayed.
+document.write("<br>");
+var Family=Family.fontcolor("red");
+var A=A.fontcolor("pink");
+document.write("<br>");
+var M=M.fontcolor("purple"); //this turns the font color of the variable of Mike and Mallory purple
+document.write("<br>");
+document.write(Family);
+document.write("<br>");
+document.write(M);
+document.write("<br>");
+document.write("3+3");
+
+document.write(A);
+
+document.write(W);
