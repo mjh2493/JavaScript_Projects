f264054f6a917cd0d7c46deccf3eaba0ecf3c6f6
mjh2493 <mjh2493@gmail.com>
2021-11-15 20:07:04 -0500

    additional challenges

--- JavaScript/JavaScript_2.js
+++ JavaScript/JavaScript_2.js
@@ -1,5 +1,5 @@
 function validateForm() {
-    var x=document.forms["myForm"]["fname", "lname", "email"].value;
+    var x=document.forms["myForm"]["fname", "lname", "email", "phone"].value;
     if (x== "") {
         alert("All fields must be filled out");
         return false;
